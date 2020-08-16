27fb240c41eea1285a8f64e233ac107d5f38617b
Anthony Morris <anthony@amorrissound.com>
2020-08-15 21:41:17 -0700

    Enhance auth UI (#5)

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,37 +1,13 @@
-import { Link, navigate } from '@redwoodjs/router'
-import { useAuth } from '@redwoodjs/auth'
-import { useState } from 'react'
+import Nav from 'src/components/Nav/Nav'
 
 const HomePage = () => {
-  const [email, setEmail] = useState('')
-  const { isAuthenticated, currentUser, logIn, logOut } = useAuth()
-  console.log(isAuthenticated, currentUser)
   return (
     <>
-      <h1 className="font-bold text-4xl">HomePage</h1>
-      <p>Find me in "./web/src/pages/HomePage/HomePage.js"</p>
-      <form action="#">
-        <input
-          type="email"
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button
-          onClick={async () => {
-            if (isAuthenticated) {
-              await logOut()
-            } else {
-              await logIn({ email })
-            }
-          }}
-        >
-          {isAuthenticated ? 'Log out' : 'Log in'}
-        </button>
-      </form>
-      <p>
-        My default route is named "home", link to me with `
-        <Link to="home">routes.home()</Link>`
-      </p>
+      <div className="container mx-auto">
+        <Nav />
+
+        <h1 className="font-bold text-4xl">Welcome!</h1>
+      </div>
     </>
   )
 }
